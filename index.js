a746da9734aecc9badc7839b8503e03569e106ea
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2023-07-22 15:29:54 +0530

    chore: new way

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 function deleteShortsFromFeed() {
-  const shorts = [...document.querySelectorAll('a[href^="/shorts/"]')]
-    ?.filter((a) => a?.id === 'thumbnail')
-    ?.map((i) => i?.parentElement?.parentElement?.parentElement?.parentComponent);
-  shorts?.forEach((short) => (short.style.display = 'none'));
+  // const shorts = [...document.querySelectorAll('a[href^="/shorts/"]')]
+  //   ?.filter((a) => a?.id === 'thumbnail')
+  //   ?.map((i) => i?.parentElement?.parentElement?.parentElement?.parentElement?.parentElement);
+
+  const shorts = [...document.querySelectorAll("[is-shorts]")]
+  shorts?.forEach((short) => {
+    short.style.display = "none"
+  })
 }
 
-window?.addEventListener?.('scroll', deleteShortsFromFeed);
+window?.addEventListener("scroll", deleteShortsFromFeed)
